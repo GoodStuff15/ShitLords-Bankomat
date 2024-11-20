d7ebb6bf62d8fcc9169139e2334c95acca59213b
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2024-11-20 11:25:49 +0100

    Add project files.

---
+++ ShitLords-Bankomat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Shitlords_Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
