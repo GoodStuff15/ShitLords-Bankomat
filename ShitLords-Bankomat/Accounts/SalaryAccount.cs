e5366096eaebcab8522651ef9adb6d48c7f11720
LinaOlandersson <olanderssonlina@gmail.com>
2024-11-21 14:08:45 +0100

    Added method ToString() in Account class. Added classes SalaryAccount and SavingsAccount as sub classes to the Account base class.

---
+++ ShitLords-Bankomat/Accounts/SalaryAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace Shitlords_Bankomat.Accounts
+{
+    internal class SalaryAccount : Account
+    {
+        public override string AccountType { get; set; } = "SalaryAccount";
+        public SalaryAccount(string accountType, decimal amount, decimal currency, int accountNumber, int ownerID) 
+            : base(accountType, amount, currency, accountNumber, ownerID)
+        {
+        }
+    }
+}
