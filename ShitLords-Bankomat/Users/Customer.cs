368c8a13adf0005545235810c3a84b54b7957828
LinaOlandersson <olanderssonlina@gmail.com>
2024-11-21 11:27:35 +0100

    Added class Admin : User. Added abstract property UserType to User class and defined the UserType in its sub classes.

--- ShitLords-Bankomat/Users/Customer.cs
+++ ShitLords-Bankomat/Users/Customer.cs
@@ -5,10 +5,11 @@
         public List<Account> accounts;
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
+        public override string UserType { get; set; } = "Customer";
 
         public bool IsBlocked { get; set; }
 
-        public Customer(string id, string passWord, string firstName, string lastName) : base(id, passWord)
+        public Customer(string id, string passWord, string firstName, string lastName, string userType) : base(id, passWord, userType)
         {
             accounts = new List<Account>();
             FirstName = firstName;
