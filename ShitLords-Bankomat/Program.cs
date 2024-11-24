3a34d1d5f2588cf4b7379d39428bea62d1d469bb
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-11-24 20:20:28 +0100

    Added switch menues to Admin and Customer class.

--- ShitLords-Bankomat/Program.cs
+++ ShitLords-Bankomat/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Shitlords_Bankomat
+﻿using Shitlords_Bankomat.Menu;
+
+namespace Shitlords_Bankomat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
