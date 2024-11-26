ff62a1044c90b316d185272b7b4973c7205e89a8
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-26 07:16:18 +0100

    Created a TransactionFactory

---
+++ Shitlords-Bankomat/TransactionFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace Shitlords_Bankomat
+{
+    public abstract class TransactionFactory
+    {
+        public abstract Transaction DoTransaction(TransactionDataHandler transactionDataHandler);
+        public Transaction CreateTransaction()
+        {
+            return this.DoTransaction(new TransactionDataHandler());
+        }
+    }
+}
